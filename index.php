e6b459e618b0f5522175c814e4a38a970f1c88a6
Ariane <ariane.auguste@design-portfolio.co.uk>
2022-04-18 21:24:46 +0100

    Updated classes and styles

--- index.php
+++ index.php
@@ -8,59 +8,53 @@
     <link rel="stylesheet" href="css/theme.css">
 </head>
 <body>
-    <div id="header" class="container-fluid">
-            <h1>Contact Form</h1>
-    </div>
-    <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-6 pt-5 description mx-auto">
-                    <p>We would love to hear from you. Please fill out your details and your message below, and we will get back to you as soon as possible:</p>
-                    <p class="desc-required">Required fields denoted by an asterisk (<span class="asterisk">*</span>).</p>
-                </div>
-                <form action="scripts/backend.php" method="get" class="needs-validation" novalidate>
-                    <div class="form-group col-sm-12 col-md-6 mx-auto">
-                        <label for="firstname">First Name</label>
-                        <span class="required">*</span>
-                        <input type="text" id="firstname" name="firstname" class="form-control" required>
-                        <div class="valid-feedback">Looks good!</div>
-                        <div class="invalid-feedback">Please enter a first name.</div>
-                    </div>
-
-                    <div class="form-group col-sm-12 col-md-6 mx-auto">
-                        <label for="lastname">Last Name</label>
-                        <span class="required">*</span>
-                        <input type="text" id="lastname" name="lastname" class="form-control" required>
-                        <div class="valid-feedback">Looks good!</div>
-                        <div class="invalid-feedback">Please enter a last name.</div>
-                    </div>
-
-                    <div class="form-group col-sm-12 col-md-6 mx-auto">
-                        <label for="email">Email Address</label>
-                        <span class="required">*</span>
-                        <input type="email" id="email" name="email" class="form-control" required>
-                        <div class="valid-feedback">Looks good!</div>
-                        <div class="invalid-feedback">Please enter a valid email address.</div>
-                    </div>
-
-                    <div class="form-group col-sm-12 col-md-6 mx-auto">
-                        <label for="phone">Contact Number</label>
-                        <input type="tel" id="phone" name="phone" class="form-control">
-                    </div>
-
-                    <div class="form-group col-sm-12 col-md-6 mx-auto">
-                        <label for="message">Message</label>
-                        <span class="required">*</span>
-                        <textarea id="message" name="message" class="form-control" placeholder="Type your message here..." rows="5"></textarea>
-                        <div class="valid-feedback">Looks good!</div>
-                        <div class="invalid-feedback">Please type your message here.</div>
-                    </div>
-
-                    <div class="col-12 col-md-6 mx-auto submit">
-                        <input type="submit" value="Submit" class="button">
-                    </div>
-                </form>
+    <header class="d-flex justify-content-center align-items-center">
+        <h1>Contact Form</h1>
+    </header>
+    <main class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-8 col-lg-6">
+                <p>We would love to hear from you. Please fill out your details and your message below, and we will get back to you as soon as possible:</p>
+                <p class="desc-required">Required fields denoted by an asterisk (<span class="required">*</span>).</p>
             </div>
+            <form action="scripts/backend.php" method="get" class="row justify-content-center needs-validation" novalidate>
+                <div class="col-sm-12 col-md-8 col-lg-6 pb-3">
+                    <label class="form-label" for="firstname">First Name<span class="required">*</span></label>
+                    <input type="text" id="firstname" name="firstname" class="form-control" required>
+                    <div class="valid-feedback">Looks good!</div>
+                    <div class="invalid-feedback">Please enter a first name.</div>
+                </div>
+
+                <div class="col-sm-12 col-md-8 col-lg-6 pb-3">
+                    <label class="form-label" for="lastname">Last Name<span class="required">*</span></label>
+                    <input type="text" id="lastname" name="lastname" class="form-control" required>
+                    <div class="valid-feedback">Looks good!</div>
+                    <div class="invalid-feedback">Please enter a last name.</div>
+                </div>
+
+                <div class="col-sm-12 col-md-8 col-lg-6 pb-3">
+                    <label class="form-label" for="email">Email Address<span class="required">*</span></label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                    <div class="valid-feedback">Looks good!</div>
+                    <div class="invalid-feedback">Please enter a valid email address.</div>
+                </div>
+
+                <div class="col-sm-12 col-md-8 col-lg-6 pb-3">
+                    <label class="form-label" for="phone">Contact Number</label>
+                    <input type="tel" id="phone" name="phone" class="form-control">
+                </div>
+
+                <div class="col-sm-12 col-md-8 col-lg-6 pb-3">
+                    <label class="form-label" for="message">Message<span class="required">*</span></label>
+                    <textarea id="message" name="message" class="form-control" placeholder="Type your message here..." rows="5"></textarea>
+                    <div class="valid-feedback">Looks good!</div>
+                    <div class="invalid-feedback">Please type your message here.</div>
+                </div>
+
+                <div class="col-sm-12 col-md-8 col-lg-6 p-5 mb-4">
+                    <input type="submit" value="Submit" class="button">
+                </div>
+            </form>
         </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>

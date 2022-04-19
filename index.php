4b2a7eb9b1f52592fa47421cc488d791533c2906
Ariane <ariane.auguste@design-portfolio.co.uk>
2022-04-19 18:15:17 +0100

    front end php

--- index.php
+++ index.php
@@ -8,6 +8,38 @@
     <link rel="stylesheet" href="css/theme.css">
 </head>
 <body>
+<?php session_start(); ?>
+<?php if (isset($_SESSION['error']) && $_SESSION['error'] === true): ?>
+    <div class="alert alert-danger" role="alert">
+        <h2>Error</h2>
+        <?php if (isset($_SESSION['errorMessage'])): ?>
+            <p><?= $_SESSION['errorMessage']; ?></p>
+            <?php unset($_SESSION['errorMessage']); ?>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['fieldErrors'])): ?>
+            <p>There were errors with the following fields:</p>
+            <ul>
+                <?php foreach($_SESSION['fieldErrors'] as $fieldError): ?>
+                    <li><?= $fieldError; ?></li>
+                <?php endforeach; ?>
+            </ul>
+            <?php unset($_SESSION['fieldErrors']); ?>
+        <?php endif; ?>
+        <?php
+            $submittedFields = $_SESSION['submittedFields'];
+            unset($_SESSION['submittedFields']);
+            unset($_SESSION['error']);
+        ?>
+    </div>
+<?php endif; ?>
+<?php if (isset($_SESSION['success']) && $_SESSION['success'] === true): ?>
+    <div class="alert alert-success" role="alert">
+        <h2>Success</h2>
+        <p>Thank you for your submission.</p>
+    </div>
+    <?php unset($_SESSION['success']); ?>
+<?php endif; ?>
+
     <header class="d-flex justify-content-center align-items-center">
         <h1>Contact Form</h1>
     </header>
@@ -23,21 +55,18 @@
                 <div class="mb-3">
                     <label class="form-label" for="firstname">First Name<span class="required">*</span></label>
                     <input type="text" id="firstname" name="firstname" class="form-control" required>
-                    <div class="valid-feedback">Looks good!</div>
                     <div class="invalid-feedback">Please enter a first name.</div>
                 </div>
 
                 <div class="mb-3">
                     <label class="form-label" for="lastname">Last Name<span class="required">*</span></label>
                     <input type="text" id="lastname" name="lastname" class="form-control" required>
-                    <div class="valid-feedback">Looks good!</div>
                     <div class="invalid-feedback">Please enter a last name.</div>
                 </div>
 
                 <div class="mb-3">
                     <label class="form-label" for="email">Email Address<span class="required">*</span></label>
                     <input type="email" id="email" name="email" class="form-control" required>
-                    <div class="valid-feedback">Looks good!</div>
                     <div class="invalid-feedback">Please enter a valid email address.</div>
                 </div>
 
@@ -49,7 +78,6 @@
                 <div class="mb-3">
                     <label class="form-label" for="message">Message<span class="required">*</span></label>
                     <textarea id="message" name="message" class="form-control" placeholder="Type your message here..." rows="5" required></textarea>
-                    <div class="valid-feedback">Looks good!</div>
                     <div class="invalid-feedback">Please type your message here.</div>
                 </div>
 
@@ -58,7 +86,6 @@
         </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<!--    example script below-->
     <script src="scripts/validator.js"></script>
 </body>
 </html>
